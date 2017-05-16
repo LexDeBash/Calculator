fd5c69371bfe6d5e86ece4bc47dab62f71bf79be
Alexey Efimov <lex.efimov@gmail.com>
2017-05-16 23:09:13 +0500

    Новые алгоритмы функций вычисления

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,20 +14,43 @@ class ViewController: UIViewController {
     
     var plusButtonOn = false
     var minusButtonOn = false
+    var multiplyButtonOn = false
+    var divisionButtonOn = false
     var newNumber = false
     var buttonOn = false
-    var lastNumber = 0
-    var tempResult = 0
-    var numberOne: Int?
-    var numberTwo: Int?
+    var result: Int?
+    var currentNuber: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func compilation() {
+        if plusButtonOn {
+            result! += currentNuber!
+        } else if minusButtonOn {
+            result! -= currentNuber!
+        } else if multiplyButtonOn {
+            result! *= currentNuber!
+        } else if divisionButtonOn {
+            result! /= currentNuber!
+        }
+    }
+    
+    func funcButton() {
+        if buttonOn == false {
+            newNumber = true
+            buttonOn = true
+            if result != nil {
+                screenNumber.text = String(result!)
+            } else {
+                result = Int(screenNumber.text!)!
+            }
+        }
+    }
+    
     @IBAction func fourButton(_ sender: UIButton) {
-        lastNumber = 0
         if screenNumber.text == "0" || newNumber {
             screenNumber.text = "4"
             newNumber = false
@@ -35,9 +58,10 @@ class ViewController: UIViewController {
         } else {
             screenNumber.text = screenNumber.text! + "4"
         }
+        currentNuber = Int(screenNumber.text!)!
     }
+    
     @IBAction func fiveButton(_ sender: UIButton) {
-        lastNumber = 0
         if screenNumber.text == "0" || newNumber {
             screenNumber.text = "5"
             newNumber = false
@@ -45,61 +69,47 @@ class ViewController: UIViewController {
         } else {
             screenNumber.text = screenNumber.text! + "5"
         }
+        currentNuber = Int(screenNumber.text!)!
     }
     
     @IBAction func plusButton(_ sender: UIButton) {
+        compilation()
         plusButtonOn = true
         minusButtonOn = false
-        if buttonOn == false {
-            newNumber = true
-            buttonOn = true
-            tempResult += Int(screenNumber.text!)!
-            screenNumber.text = String(tempResult)
-        }
+        multiplyButtonOn = false
+        divisionButtonOn = false
+        funcButton()
     }
     
     @IBAction func minusButton(_ sender: UIButton) {
-        minusButtonOn = true
+        compilation()
         plusButtonOn = false
-        if buttonOn == false {
-            newNumber = true
-            buttonOn = true
-            if numberOne == nil {
-                numberOne = Int(screenNumber.text!)!
-                tempResult = numberOne!
-            } else if numberTwo == nil {
-                numberTwo = Int(screenNumber.text!)!
-                tempResult = numberOne! - numberTwo!
-            } else {
-                tempResult -= Int(screenNumber.text!)!
-            }
-            
-            screenNumber.text = String(tempResult)
-        }
+        minusButtonOn = true
+        multiplyButtonOn = false
+        divisionButtonOn = false
+        funcButton()
+    }
+    
+    @IBAction func multiplyButton(_ sender: UIButton) {
+        compilation()
+        plusButtonOn = false
+        minusButtonOn = false
+        multiplyButtonOn = true
+        divisionButtonOn = false
+        funcButton()
+    }
+    
+    @IBAction func divisionButton(_ sender: UIButton) {
+        compilation()
+        plusButtonOn = false
+        minusButtonOn = false
+        multiplyButtonOn = false
+        divisionButtonOn = true
+        funcButton()
     }
     
     @IBAction func equalButton(_ sender: UIButton) {
-        newNumber = true
-        buttonOn = true
-        if lastNumber != 0 {
-            if plusButtonOn {
-                tempResult += lastNumber
-                screenNumber.text = String(tempResult)
-            } else if minusButtonOn {
-                tempResult -= lastNumber
-                screenNumber.text = String(tempResult)
-            }
-            
-        } else {
-            lastNumber = Int(screenNumber.text!)!
-            if plusButtonOn {
-                tempResult += lastNumber
-                screenNumber.text = String(tempResult)
-            } else if minusButtonOn {
-                tempResult -= lastNumber
-                screenNumber.text = String(tempResult)
-            }
-        }
+
     }
 
 
