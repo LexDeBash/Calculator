70e7ea8200f5671cce0ba0bb9183e3d3cdef63e7
Alexey Efimov <lex.efimov@gmail.com>
2017-05-22 21:02:37 +0500

    Версия SwiftBook.ru

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayResultLabel: UILabel!
     
+    var stillTyping = false
+    var dotIsPlaced = false
+    var firstOperand: Double = 0
+    var secondOperand: Double = 0
+    var operationSing: String = ""
+    
     var operatorsArray: [String] = []
     var lastOperator: String = ""
     var equalButtonOn = false
@@ -31,33 +37,48 @@ class ViewController: UIViewController {
             } else {
                 displayResultLabel.text = "\(newValue)"
             }
+            stillTyping = false
         }
     }
     
     @IBAction func pressedDigitalButton(_ sender: UIButton) {
-        let number = sender.currentTitle
+        let number = sender.currentTitle!
         
-        guard (displayResultLabel.text?.characters.count)! < 19 else { return }
-        
-        if displayResultLabel.text == "0" || newValue {
-            displayResultLabel.text = number
-            newValue = false
-            operatorButtonOn = false
+        if stillTyping {
+            if (displayResultLabel.text?.characters.count)! < 20 {
+                displayResultLabel.text = displayResultLabel.text! + number
+            }
         } else {
-            displayResultLabel.text = displayResultLabel.text! + number!
+            displayResultLabel.text = number
         }
-        currentOperand = currentInput
-        print("digitalButton: secondOperand = \(currentOperand!)")
+        stillTyping = true
+        
+//        guard (displayResultLabel.text?.characters.count)! < 19 else { return }
+//        
+//        if displayResultLabel.text == "0" || newValue {
+//            displayResultLabel.text = number
+//            newValue = false
+//            operatorButtonOn = false
+//        } else {
+//            displayResultLabel.text = displayResultLabel.text! + number!
+//        }
+//        currentOperand = currentInput
+//        print("digitalButton: secondOperand = \(currentOperand!)")
     }
     
     @IBAction func operatorKey(_ sender: UIButton) {
-        lastOperator = ""
-        compilation()
-        saveValue()
-        equalButtonOn = false
-        lastOperator = sender.currentTitle!
-        print("Key \(lastOperator)")
-        operatorsArray.append(lastOperator)
+//        lastOperator = ""
+//        compilation()
+//        saveValue()
+//        equalButtonOn = false
+//        lastOperator = sender.currentTitle!
+//        print("Key \(lastOperator)")
+//        operatorsArray.append(lastOperator)
+        
+        operationSing = sender.currentTitle!
+        firstOperand = currentInput
+        stillTyping = false
+        dotIsPlaced = false
     }
     
     @IBAction func registrChangeKey(_ sender: UIButton) {
@@ -66,110 +87,155 @@ class ViewController: UIViewController {
     }
     
     @IBAction func percentKey(_ sender: UIButton) {
-        guard savedValue != nil else { return }
-        if operatorsArray.last == "×" {
-            compilation()
-            savedValue! /= 100
-        } else if operatorsArray.last == "÷" {
-            compilation()
-            savedValue! *= 100
-        }else {
-            currentOperand = savedValue!/100 * currentOperand!
-            compilation()
+        if firstOperand == 0 {
+            currentInput /= 100
+        } else {
+            secondOperand = firstOperand * currentInput / 100
         }
-        currentInput = savedValue!
-        operatorsArray = []
-        newValue = true
-        equalButtonOn = true
+        
+        stillTyping = false
+//        guard savedValue != nil else { return }
+//        if operatorsArray.last == "×" {
+//            compilation()
+//            savedValue! /= 100
+//        } else if operatorsArray.last == "÷" {
+//            compilation()
+//            savedValue! *= 100
+//        }else {
+//            currentOperand = savedValue!/100 * currentOperand!
+//            compilation()
+//        }
+//        currentInput = savedValue!
+//        operatorsArray = []
+//        newValue = true
+//        equalButtonOn = true
     }
     
     @IBAction func squareRootKey(_ sender: UIButton) {
         currentInput = sqrt(currentInput)
-        newValue = true
-        savedValue = nil
+//        newValue = true
+//        savedValue = nil
+    }
+    
+    func equalitySingPressed(operation: (Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperand)
+        stillTyping = false
     }
     
     @IBAction func equalButton(_ sender: UIButton) {
-        print("РАВНО")
-        guard !equalButtonOn else {
-            compilation() // При повторном нажатии на равно
-            operatorButtonOn = false
-            print("Равно: Сработал первый guard и число на экране равно savedValue, т.е. \(savedValue!)")
-            return currentInput = savedValue!
-        }
-        equalButtonOn = true
-        guard !operatorButtonOn else {
-            compilation() // При активной клавише оператора
-            print("Равно: Сработал второй guard и число на экране равно savedValue, т.е. \(savedValue!)")
-            return currentInput = savedValue!
+        if stillTyping {
+            secondOperand = currentInput
         }
         
-        guard savedValue != nil else { return }
-        compilation() // Если клавиша оператора и клавиша равно не активны
-        currentInput = savedValue!
-        operatorsArray = []
-        newValue = true
-        print("Равно: Сработал третий guard и число на экране равно savedValue, т.е. \(savedValue!)")
+        dotIsPlaced = false
+        
+        switch operationSing {
+        case "+":
+            equalitySingPressed{$0 + $1}
+        case "-":
+            equalitySingPressed{$0 - $1}
+        case "×":
+            equalitySingPressed{$0 * $1}
+        case "÷":
+            equalitySingPressed{$0 / $1}
+        default: break
+        }
+//        print("РАВНО")
+//        guard !equalButtonOn else {
+//            compilation() // При повторном нажатии на равно
+//            operatorButtonOn = false
+//            print("Равно: Сработал первый guard и число на экране равно savedValue, т.е. \(savedValue!)")
+//            return currentInput = savedValue!
+//        }
+//        equalButtonOn = true
+//        guard !operatorButtonOn else {
+//            compilation() // При активной клавише оператора
+//            print("Равно: Сработал второй guard и число на экране равно savedValue, т.е. \(savedValue!)")
+//            return currentInput = savedValue!
+//        }
+//        
+//        guard savedValue != nil else { return }
+//        compilation() // Если клавиша оператора и клавиша равно не активны
+//        currentInput = savedValue!
+//        operatorsArray = []
+//        newValue = true
+//        print("Равно: Сработал третий guard и число на экране равно savedValue, т.е. \(savedValue!)")
     }
     
     @IBAction func resetButton(_ sender: UIButton) {
-        operatorsArray = []
-        equalButtonOn = false
-        newValue = false
-        operatorButtonOn = false
-        savedValue = nil
-        currentOperand = nil
-        displayResultLabel.text = "0"
+        firstOperand = 0
+        secondOperand = 0
         currentInput = 0
+        displayResultLabel.text = "0"
+        stillTyping = false
+        dotIsPlaced = false
+        operationSing = ""
+//        operatorsArray = []
+//        equalButtonOn = false
+//        newValue = false
+//        operatorButtonOn = false
+//        savedValue = nil
+//        currentOperand = nil
+//        displayResultLabel.text = "0"
+//        currentInput = 0
     }
     
-    func compilation() {
-        // Отключаем многократное нажатие на функциональные клавиши, за исключением равно
-        guard !operatorButtonOn || equalButtonOn else {
-            print("Compilation: First guard")
-            return
+    @IBAction func dotButtonPressed(_ sender: UIButton) {
+        if stillTyping && !dotIsPlaced {
+            displayResultLabel.text = displayResultLabel.text! + "."
+            dotIsPlaced = true
+        } else if !stillTyping && !dotIsPlaced {
+            displayResultLabel.text = "0."
         }
-        
-        // Выходим из функции при пустом массиве операторов или если последний оператор не сохранен
-        guard !operatorsArray.isEmpty || lastOperator != "" else {
-            print("Compilation: Second guard")
-            return
-        }
-        
-        var _operatorButton = ""
-        if lastOperator != "" {
-            _operatorButton = lastOperator // Извлекаем сохраненный оператор
-        } else {
-            _operatorButton = operatorsArray.last! // Если сорхраненного оператора нет, берем последний оператор из массива
-        }
-        switch true {
-        case _operatorButton == "+":
-            savedValue! += currentOperand!
-        case _operatorButton == "-":
-            savedValue! -= currentOperand!
-        case _operatorButton == "×":
-            savedValue! *= currentOperand!
-        case _operatorButton == "÷":
-            savedValue! /= currentOperand!
-        default:
-            print("Compilation false")
-            break
-        }
-        print("Compilation: saveValue = \(savedValue!), secondOperand = \(currentOperand!)")
     }
     
-    func saveValue() {
-        if !operatorButtonOn {
-            newValue = true
-            operatorButtonOn = true
-            if savedValue != nil && !equalButtonOn {
-                currentInput = savedValue!
-                print("saveValue: displayResultLabel = savedValue = \(savedValue!)")
-            } else {
-                savedValue = currentInput
-                print("saveValue: savedValue = displayResultLabel = \(savedValue!)")
-            }
-        }
-    }
+//    func compilation() {
+//        // Отключаем многократное нажатие на функциональные клавиши, за исключением равно
+//        guard !operatorButtonOn || equalButtonOn else {
+//            print("Compilation: First guard")
+//            return
+//        }
+//        
+//        // Выходим из функции при пустом массиве операторов или если последний оператор не сохранен
+//        guard !operatorsArray.isEmpty || lastOperator != "" else {
+//            print("Compilation: Second guard")
+//            return
+//        }
+//        
+//        var _operatorButton = ""
+//        if lastOperator != "" {
+//            _operatorButton = lastOperator // Извлекаем сохраненный оператор
+//        } else {
+//            _operatorButton = operatorsArray.last! // Если сорхраненного оператора нет, берем последний оператор из массива
+//        }
+//        switch true {
+//        case _operatorButton == "+":
+//            savedValue! += currentOperand!
+//        case _operatorButton == "-":
+//            savedValue! -= currentOperand!
+//        case _operatorButton == "×":
+//            savedValue! *= currentOperand!
+//        case _operatorButton == "÷":
+//            savedValue! /= currentOperand!
+//        default:
+//            print("Compilation false")
+//            break
+//        }
+//        print("Compilation: saveValue = \(savedValue!), secondOperand = \(currentOperand!)")
+//    }
+//    
+//    func saveValue() {
+//        if !operatorButtonOn {
+//            newValue = true
+//            operatorButtonOn = true
+//            if savedValue != nil && !equalButtonOn {
+//                currentInput = savedValue!
+//                print("saveValue: displayResultLabel = savedValue = \(savedValue!)")
+//            } else {
+//                savedValue = currentInput
+//                print("saveValue: savedValue = displayResultLabel = \(savedValue!)")
+//            }
+//        }
+//    }
 }
 
